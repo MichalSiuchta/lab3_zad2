6111fc466784a58cd28d45cffba3ad3cbd373a72
Michał <michal.siucht4@gmail.com>
2024-12-19 09:03:56 +0100

    wejscie

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
-    printf("Hello, World!\n");
+    char zdanie[200] = {0};
+    srand(time(0));
+    printf("Podaj zdanie:");
+    fgets(zdanie, 200, stdin);
+    int klucz = rand()%25 + 1;
+    printf("Zdanie: %s \n",zdanie);
+    printf("Zdanie zakodowane: ");
+
+
+
     return 0;
 }
