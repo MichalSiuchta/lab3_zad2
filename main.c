9fac6c85f0e7b83e06e4f09b9420e535cc753189
Michał <michal.siucht4@gmail.com>
2024-12-19 14:58:07 +0100

    Final

--- main.c
+++ main.c
@@ -1,17 +1,31 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 int main() {
-    char zdanie[200] = {0};
+    char zdanie[200];
     srand(time(0));
     printf("Podaj zdanie:");
     fgets(zdanie, 200, stdin);
     int klucz = rand()%25 + 1;
-    printf("Zdanie: %s \n",zdanie);
+    printf("Zdanie: %s",zdanie);
     printf("Zdanie zakodowane: ");
 
+    int i = 0;
 
+    while(zdanie[i] != '\0') {
+        if(islower(zdanie[i])) {
+            printf("%c", (zdanie[i] - 'a' + klucz) %26 + 'a');
+        }else
+        if(isupper(zdanie[i])) {
+            printf("%c", (zdanie[i] - 'A' + klucz) %26 + 'A');
+        }else {
+            printf("%c", zdanie[i]);
+        }
+
+        i++;
+    }
 
     return 0;
 }
